4280ae42b27dfbca546f2571714bbbc50a3658e3
Monah Haidar <mnhaidar01@gmail.com>
2025-04-06 21:12:41 +0300

    Implement UserCard Component

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,54 @@
+import PrimaryButton from "./atoms/PrimaryButton.tsx";
+import DangerButton from "./atoms/DangerButton.tsx";
+
+export enum Status {
+    Active = 'active',
+    Locked = 'locked',
+}
+
+interface Props {
+    className?: string;
+    name: string;
+    email: string;
+    status: Status;
+    dateOfBirth: Date;
+}
+
+const UserCard = ({className = '', name, email, status, dateOfBirth}: Props) => {
+
+    const getInitials = (name: string) => {
+        const [first = '', last = ''] = name.trim().split(' ');
+        return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase();
+    };
+
+
+    return (
+        <div className={`flex flex-col w-full bg-red-50 p-4 bg-white shadow-lg rounded-md gap-4` + className}>
+            <div>
+                <div className={`flex items-center justify-center`}>
+                    <span
+                        className={`p-4 w-16 h-16 flex items-center justify-center text-xl font-bold text-white rounded-full bg-primary`}>
+                        {getInitials(name)}
+                    </span>
+                </div>
+
+                <div className={`flex flex-col gap-1`}>
+                    <p className={`text-xl font-semibold text-gray-800`}>{name}</p>
+                    <p className={`text-xs font-semibold text-gray-500`}>Email: {email}</p>
+                    <p className={`text-xs font-semibold text-gray-500`}>Status: {status}</p>
+                    <p className={`text-xs font-semibold text-gray-500`}>Date of
+                        Birth: {dateOfBirth.toLocaleDateString()}</p>
+                </div>
+
+            </div>
+
+
+            <div className={`flex items-center gap-x-2 justify-end`}>
+                <PrimaryButton className={``}>Edit</PrimaryButton>
+                <DangerButton className={``}>Delete</DangerButton>
+            </div>
+        </div>
+    );
+};
+
+export default UserCard;
