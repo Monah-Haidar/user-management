e2298d3834b433b6649e23091a0d579934eb1ea6
Monah Haidar <mnhaidar01@gmail.com>
2025-04-06 20:05:12 +0300

    Modify Navbar Component. Remove button Components.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-import PrimaryButton from "./atoms/PrimaryButton.tsx";
-import DangerButton from "./atoms/DangerButton.tsx";
 import {IoMoonOutline} from "react-icons/io5";
 import {IoMenu} from "react-icons/io5";
 
@@ -11,8 +9,19 @@ const Navbar = () => {
         <nav className={`py-4 px-6 h-16 flex items-center justify-between bg-primary`}>
             <h1 className={`text-white text-xl`}>User Management</h1>
             <div className="hidden sm:flex items-center justify-between gap-4">
-                <PrimaryButton className={``}>Create User</PrimaryButton>
-                <DangerButton className={``}>Logout</DangerButton>
+                <button
+                    className={
+                        `inline-flex items-center rounded-sm border border-transparent bg-white px-4 py-2 text-sm font-semibold text-primary cursor-pointer transition duration-150 ease-in-out hover:bg-neutral-400`}
+                >
+                    Create User
+                </button>
+                <button
+                    className={
+                        `inline-flex items-center rounded-sm border border-transparent bg-red-600 px-4 py-2 text-sm font-semibold text-white cursor-pointer transition duration-150 ease-in-out hover:bg-red-400 hover:text-white`}
+                >
+                    Logout
+                </button>
+
                 <span className={`px-4 py-2 cursor-pointer`}><IoMoonOutline className={`text-white`}/></span>
             </div>
             <div className={`cursor-pointer flex sm:hidden`}><IoMenu className={`text-white text-3xl`}/></div>
