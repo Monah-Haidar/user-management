33a31d34acfd0a7b88dbf03b90a78ee0383eae3f
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-26 13:47:28 +0300

    React Query Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css';
-import {ThemeProvider, useThemeStore} from "./stores/themeStore";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import { ThemeProvider, useThemeStore } from "./stores/themeStore";
 import AppRouter from "./routing/AppRouter.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const theme = useThemeStore.getState().theme;
+document.documentElement.classList.remove("light", "dark");
+document.documentElement.classList.add(theme);
 
+const queryClient = new QueryClient();
 
-const theme = useThemeStore.getState().theme
-document.documentElement.classList.remove('light', 'dark')
-document.documentElement.classList.add(theme)
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <ThemeProvider>
-        <AppRouter />
-      </ThemeProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <AppRouter />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
+        </ThemeProvider>
+    </StrictMode>
+);
