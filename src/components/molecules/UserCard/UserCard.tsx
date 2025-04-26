a22ac558637bd96e21fad3084cc3bf9ffe42e983
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-26 23:49:29 +0300

    Update User Card to send data to Edit User Form

--- src/components/molecules/UserCard/UserCard.tsx
+++ src/components/molecules/UserCard/UserCard.tsx
@@ -1,39 +1,81 @@
-import {PrimaryButton} from "../../atoms/PrimaryButton";
-import {DangerButton} from "../../atoms/DangerButton";
+import { PrimaryButton } from "../../atoms/PrimaryButton";
+import { DangerButton } from "../../atoms/DangerButton";
 import { UserCardProps } from "./UserCard.type.ts";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-
-const UserCard = ({className = '', firstName, lastName, email, status, dateOfBirth}: UserCardProps) => {
-
-    const getInitials = (firstName: string = '', lastName: string = '') => {
+const UserCard = ({
+    className = "",
+    id,
+    firstName,
+    lastName,
+    email,
+    status,
+    dateOfBirth,
+}: UserCardProps) => {
+    const getInitials = (firstName: string = "", lastName: string = "") => {
         return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
     };
 
- 
+    const userData = {
+        id,
+        firstName,
+        lastName,
+        email,
+        status,
+        dateOfBirth,
+    };
 
     return (
-        <div className={`flex flex-col w-full p-4 bg-white shadow-lg rounded-md gap-4 dark:bg-gray-800 dark:text-gray-100 ` + className}>
+        <div
+            className={
+                `flex flex-col w-full p-4 bg-white shadow-lg rounded-md gap-4 dark:bg-gray-800 dark:text-gray-100 ` +
+                className
+            }
+        >
             <div>
                 <div className={`flex items-center justify-center`}>
                     <span
-                        className={`p-4 w-16 h-16 flex items-center justify-center text-xl font-bold text-white rounded-full bg-primary`}>
+                        className={`p-4 w-16 h-16 flex items-center justify-center text-xl font-bold text-white rounded-full bg-primary`}
+                    >
                         {getInitials(firstName, lastName)}
                     </span>
                 </div>
 
                 <div className={`flex flex-col gap-1`}>
-                    <p className={`text-xl font-semibold text-gray-800 dark:text-gray-200`}>{firstName} {lastName}</p>
-                    <p className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}>Email: {email}</p>
-                    <p className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}>Status: {status}</p>
-                    <p className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}>Date of
-                        Birth: {dateOfBirth.toLocaleString()}</p>
+                    <p
+                        className={`text-xl font-semibold text-gray-800 dark:text-gray-200`}
+                    >
+                        {firstName} {lastName}
+                    </p>
+                    <p
+                        className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}
+                    >
+                        Email: {email}
+                    </p>
+                    <p
+                        className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}
+                    >
+                        Status: {status}
+                    </p>
+                    <p
+                        className={`text-xs font-semibold text-gray-500 dark:text-gray-400 `}
+                    >
+                        Date of Birth: {dateOfBirth.toLocaleString()}
+                    </p>
                 </div>
             </div>
 
             <div className={`flex items-center gap-x-2 justify-end`}>
-                <PrimaryButton className={``}>Edit</PrimaryButton>
-                <DangerButton className={``}>Delete</DangerButton>
+                <NavLink 
+                    to={`/dashboard/edit/${id}`}
+                    state={{ user: userData }}
+                >
+                    <PrimaryButton>Edit</PrimaryButton>
+                </NavLink>
+                <NavLink to={``}>
+                    <DangerButton>Delete</DangerButton>
+                </NavLink>
             </div>
         </div>
     );
